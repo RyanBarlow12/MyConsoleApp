c49c7241a4103ca9a86d433b32ac06dd9907c6cf
Ryan Barlow <barlow.188@osu.edu>
2024-01-25 08:39:58 -0500

    Commit is an update

--- MyConsoleApp1/obj/Debug/net8.0/MyConsoleApp1.AssemblyInfo.cs
+++ MyConsoleApp1/obj/Debug/net8.0/MyConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8334bafa7eb64046fd5f6d3b5d42c1c02a01a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
